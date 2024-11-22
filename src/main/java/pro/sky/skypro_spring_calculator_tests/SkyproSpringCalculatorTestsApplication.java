6a49b6f896b09e0b0e54b745391c0d7a9c626b3f
anny-sit <annysit@yandex.ru>
2024-11-22 18:02:15 +0300

    initial commit

---
+++ src/main/java/pro/sky/skypro_spring_calculator_tests/SkyproSpringCalculatorTestsApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.skypro_spring_calculator_tests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproSpringCalculatorTestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproSpringCalculatorTestsApplication.class, args);
+	}
+
+}
